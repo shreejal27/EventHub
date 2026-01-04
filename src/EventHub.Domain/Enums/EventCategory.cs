e66419951d87baffe3bd625f71e67cf57ceedda8
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-04 23:57:33 +0545

    made enum files

---
+++ src/EventHub.Domain/Enums/EventCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace EventHub.Domain.Enums
+{
+    public enum EventCategory
+    {
+        Unknown = 0,
+        Music = 1,
+        Technology = 2,
+        Sports = 3,
+        Arts = 4,
+        FoodAndDrink = 5,
+        Business = 6,
+        Education = 7,
+        Community = 8,
+        Entertainment = 9,
+        HealthAndWellness = 10
+    }
+}
---
+++ src/EventHub.Domain/Enums/EventStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace EventHub.Domain.Enums
+{
+    public enum EventStatus
+    {
+        Draft = 0,
+        Active = 1,
+        Cancelled = 2,
+        Completed = 3,
+        Postponed = 4
+    }
+}
