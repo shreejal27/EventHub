d265142f7b491b6cb3ea512f2f7782e5c6327881
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-07 22:55:59 +0545

    created interface for event repository

---
+++ src/EventHub.Domain/Interfaces/IEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using EventHub.Domain.Entities;
+using EventHub.Domain.Enums;
+
+namespace EventHub.Domain.Interfaces;
+
+public interface IEventRepository
+{
+    Task<Event?> GetByIdAsync(int id);
+    Task<List<Event>> GetAllAsync();
+    Task<List<Event>> GetByCategoryAsync(EventCategory category);
+    Task<List<Event>> GetByStatusAsync(EventStatus status);
+    Task<List<Event>> GetUpcomingEventsAsync();
+    Task<List<Event>> GetByCityAsync(string city);
+    Task<Event> AddAsync(Event @event);
+    Task AddRangeAsync(List<Event> events);
+    Task UpdateAsync(Event @event);
+    Task DeleteAsync(Event @event);
+    Task<bool> ExistsAsync(int id);
+    Task<int> CountAsync();
+}
