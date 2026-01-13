730f5bedae771c62173af054bbfd5c87aeb01f9a
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-13 23:58:51 +0545

    added configuration and db context files

---
+++ src/EventHub.Infrastructure/Persistence/EventHubDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EventHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHub.Infrastructure.Persistence;
+public class EventHubDbContext : DbContext
+{
+    public DbSet<Event> Events { get; set; } = null!;
+
+   
+    public EventHubDbContext(DbContextOptions<EventHubDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(EventHubDbContext).Assembly);
+
+    }
+}
