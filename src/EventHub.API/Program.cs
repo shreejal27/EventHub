c917a5297c6a5b3528e788d4af638b82e5f0a679
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-14 23:52:04 +0545

    modified program file

--- src/EventHub.API/Program.cs
+++ src/EventHub.API/Program.cs
@@ -1,23 +1,40 @@
+using EventHub.Domain.Interfaces;
+using EventHub.Infrastructure.Persistence;
+using EventHub.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<EventHubDbContext>(options =>
+{
+
+    options.UseSqlite("Data Source=eventhub.db");
+
+    if (builder.Environment.IsDevelopment())
+    {
+        options.EnableSensitiveDataLogging();
+        options.EnableDetailedErrors();
+    }
+});
+
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
