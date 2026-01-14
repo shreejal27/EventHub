4851d4da850b23b12c81f8543718f63673ce9d64
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-14 23:58:52 +0545

    added new Event Controller

---
+++ src/EventHub.API/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
