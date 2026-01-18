c5cc2154bddaf6c3e9981e9803d5f74d1e1b19cd
Shreejal Maharjan <shreejal27@gmail.com>
2026-01-18 23:43:19 +0545

    added IEventScraper interface

---
+++ src/EventHub.Domain/Interfaces/IEventScraper.cs
@@ -0,0 +1,12 @@
+﻿using EventHub.Domain.Entities;
+
+namespace EventHub.Domain.Interfaces;
+
+public interface IEventScraper
+{
+    string SourceName { get; }
+    
+    Task<List<Event>> ScrapeAsync(CancellationToken cancellationToken = default);
+
+    Task<bool> IsHealthyAsync(CancellationToken cancellationToken = default);
+}
